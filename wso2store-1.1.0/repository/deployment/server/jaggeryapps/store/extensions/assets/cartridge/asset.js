640e784a2988b39ba6ec7c55d00788ba0c0e77eb
Ayesha <ayesha@puppet.test.org>
2014-06-02 11:27:13 +0530

    lifecycle added

---
+++ wso2store-1.1.0/repository/deployment/server/jaggeryapps/store/extensions/assets/cartridge/asset.js
@@ -0,0 +1,17 @@
+var assetLinks = function (user) {
+    return {
+        title: 'Apache Stratos Cartridges',
+	links: [
+            {
+                title: 'E-Books',
+                url: ''
+            },
+            {
+                title: 'Bookmarks',
+                url: 'bookmarks',
+                path: 'bookmarks.jag'
+            }
+        ],
+        isCategorySupport: true
+    };
+};
